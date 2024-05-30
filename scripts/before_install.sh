45a5b3dd95c20351659e64077c74250e3dabb0db
1ucra <kwanay1010@naver.com>
2024-05-30 14:48:45 +0900

    asdasdsad

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,16 +2,13 @@
 sudo su
 echo "BeforeInstall script started"
 
-amazon-linux-extras enable python3.8
-yum install python3.8 -y
-alternatives --set python /usr/bin/python3.8
-python3.8 -m ensurepip --upgrade
+python3 -m ensurepip --upgrade
 
 yum install mysql -y
-yum install mysql-devel -y
-yum install python38-devel -y
-python3.8 -m pip install pymysql
-export MYSQLCLIENT_CFLAGS=$(/bin/mysql_config --cflags)
-export MYSQLCLIENT_LDFLAGS=$(/bin/mysql_config --libs)
+#yum install mysql-devel -y
+yum install python-devel -y
+#python3 -m pip install pymysql
+# export MYSQLCLIENT_CFLAGS=$(/bin/mysql_config --cflags)
+# export MYSQLCLIENT_LDFLAGS=$(/bin/mysql_config --libs)
 
 echo "BeforeInstall script completed"
