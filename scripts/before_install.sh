31bf70a7509c32f8a07abbc1c356fbd2ed75494e
1ucra <kwanay1010@naver.com>
2024-05-30 16:46:01 +0900

    sadsad

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,7 +8,7 @@ sudo systemctl start nginx
 sudo systemctl enable nginx
 
 #yum install mysql -y
-#yum install mysql-devel -y
+yum install mysql-devel -y
 yum install python-devel -y
 #python3 -m pip install pymysql
 # export MYSQLCLIENT_CFLAGS=$(/bin/mysql_config --cflags)
