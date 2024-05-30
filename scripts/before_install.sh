eec924555d77ab4bcb774fa1c47c0f4a04120c3e
1ucra <kwanay1010@naver.com>
2024-05-30 14:21:41 +0900

    adas

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 sudo su
 echo "BeforeInstall script started"
+
 amazon-linux-extras enable python3.8
-yum install python3.8
+yum install python3.8 -y
 alternatives --set python /usr/bin/python3.8
 python3.8 -m ensurepip --upgrade
-yum install mariadb -y
-yum install mariadb-devel -y
+
+yum install mysql -y
+yum install mysql-devel -y
 yum install python38-devel -y
 python3.8 -m pip install pymysql
 export MYSQLCLIENT_CFLAGS=$(/bin/mysql_config --cflags)
 export MYSQLCLIENT_LDFLAGS=$(/bin/mysql_config --libs)
+
 echo "BeforeInstall script completed"
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,6 +5,6 @@ sudo su
 cd /hellowaws-application-sourcecode/django-community-board-main/
 
 systemctl restart nginx
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8080
+python3.8 manage.py makemigrations
+python3.8 manage.py migrate
+python3.8 manage.py runserver 0.0.0.0:8080
