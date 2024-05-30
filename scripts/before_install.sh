c5bc286e0165306c0ea186f417b4e2cf8e88704c
1ucra <kwanay1010@naver.com>
2024-05-30 11:27:16 +0900

    asdas

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "BeforeInstall script started"
+sudo yum install mariadb-client -y
 rm -rf /hellowaws-application-sourcecode/django-community-board-main/*
 echo "BeforeInstall script completed"
