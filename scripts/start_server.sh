f436062f1f3295387636030bcee7525a47ecffc1
1ucra <kwanay1010@naver.com>
2024-05-30 15:51:03 +0900

    error modi

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,6 +3,10 @@
 sudo su
 
 cd /hellowaws-application-sourcecode/django-community-board-main/
+python3 -m venv venv
+source venv/bin/activate
+
+python3 -m pip install --upgrade pip
 python3 -m pip install --no-cache-dir -r ./requirements.txt
 
 systemctl restart nginx
